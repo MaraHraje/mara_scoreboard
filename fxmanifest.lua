7896df842a7885f0c1d672b3c3a748428ae1dbbf
Mára <101827573+MaraHraje@users.noreply.github.com>
2024-11-17 13:02:17 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,19 +6,7 @@ description 'Jednoduchý scoreboard pro FiveM server'
 version '1.0.0'
 
 
-ui_page 'html/scoreboard.html'
-
-files {
-    'html/scoreboard.html',
-    'html/scoreboard.css',
-    'html/scoreboard.js',
-    'images/cina.png',       -- Přidejte všechny potřebné obrázky jobů
-    'images/cesko.png',
-    'images/rudaarmada.png',
-    'images/spojenestaty.png',
-    'images/britanie.png',
-    'images/*.png'
-}
+ui_page 'html/*'
 
 client_scripts {
     'client.lua'
